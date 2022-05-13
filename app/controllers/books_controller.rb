e1f757d7456fda5cd1c934d7fd4bf2a6fe9c1b33
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-05-13 13:01:21 +0000

    [Create]3-exercise_2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,35 @@ class BooksController < ApplicationController
     @book=Book.new(book_params)
   end
 
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
