3d193b80cfccbc197e925a77e244fa6536c33ebe
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-05-16 14:24:35 +0000

    [Finish]3-exercise

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,12 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
+      flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books=Book.all
       render:index
     end
-    flash[:success]='Book was successfully created.'
   end
 
   def show
@@ -23,20 +24,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
       render:edit
     end
-    flash[:success]='Book was successfully updated.'
   end
 
   def destroy
     book=Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
-    flash[:success]='Book was successfully destroyed.'
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
